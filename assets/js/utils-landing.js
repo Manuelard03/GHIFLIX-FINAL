b69d54f81c3b194dbfcd64440f3ad4f3d92a3e17
Manuela <manuelarodriguezdavila@outlook.com>
2023-06-07 10:38:25 -0500

    commit 2

---
+++ assets/js/utils-landing.js
@@ -0,0 +1,26 @@
+export class Pelicula {
+    titulo = ""
+    banner = ""
+    tarjeta = ""
+    overview = ""
+
+    constructor (title_banner, banner, card, synopsis){
+        this.titulo = title_banner;
+        this.banner = banner;
+        this.tarjeta = card;
+        this.overview = synopsis;
+    }
+
+    render() {
+        const card = document.createElement('div')
+        const a = document.createElement('a')
+        const img = document.createElement('img')
+        card.classList.add ("pelicula")
+        img.src = this.tarjeta;
+        a.href = "#";
+        card.appendChild(a);
+        a.appendChild(img);
+        return card;
+    }
+
+}
