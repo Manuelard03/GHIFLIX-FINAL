f26400f117074b81fccdd2dfe4f34b6c8fbdba39
Manuela <manuelarodriguezdavila@outlook.com>
2023-06-07 23:35:00 -0500

    commit 7

--- assets/js/home.js
+++ assets/js/home.js
@@ -73,189 +73,59 @@ const fila = document.querySelector('.imagenes');
             modal.classList.remove('modal--show');
         });
 
-/*fotos*/
-const carruseles = document.querySelectorAll(".carousel");
-const divs = Array.from(carruseles);
+/*Primer Carrusel*/
+const primercarrusel = document.querySelectorAll(".imagenes");
+const divs = Array.from(primercarrusel);
 
 class Pelicula {
-  titulo = "";
-  banner = "";
-  tarjeta = "";
-  overview = "";
-
-  constructor(card, banner, id, banner_title, synopsis) {
-    this.tarjeta = card;
-    this.banner = banner;
-    this.id = id;
-    this.banner_title = banner_title;
-    this.synopsis = synopsis;
-  }
-
-  render() {
-    const card = document.createElement("div");
-
-    const a = document.createElement("a");
-    const img = document.createElement("img");
-    const boton = document.createElement("button");
-    const añadir = document.createElement("button");
-    const rating = document.createElement("div");
-
-    boton.innerHTML = `
-        <i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i
-        class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i><i
-        class="fa-regular fa-star"></i>
-      `;
-    añadir.innerHTML = `
-        <i class="fa-regular fa-bookmark"></i> Añadir
-      `;
-    card.classList.add("pelicula");
-    card.id = this.id;
-    rating.classList.add("rating");
-    img.src = this.tarjeta;
-    a.href = "#";
-    card.appendChild(a);
-    a.appendChild(img);
-    rating.appendChild(boton);
-    rating.appendChild(añadir);
-    card.appendChild(rating);
-
-    return card;
-  }
-}
-
-const peliRandom = (peliculas) => {
-  return peliculas[Math.floor(Math.random() * peliculas.length)];
-}
-
-const mifuncion = async () => {
-  const response = await fetch(
-    "https://raw.githubusercontent.com/matilocker/Lab-10/main/movies.json"
-  );
-  const data = await response.json();
-  const carruseles = document.querySelectorAll(".carousel");
-  const favoritos = document.querySelectorAll(".carousel_favourites");
-  const mainMovie = document.querySelector('.main-movie');
+    titulo = "";
+    tarjeta = "";
+    
+    constructor(card, id) {
+      this.tarjeta = card;
+      this.id = id;
+    }
   
-  /*function seleccionarPeliculaAleatoria(peliculas) {
-    return peliculas[Math.floor(Math.random() * peliculas.length)];
+    render() {
+      const card = document.createElement("div");
+      const a = document.createElement("a");
+      const img = document.createElement("img");
+      card.classList.add("pelicula");
+      card.id = this.id;
+      img.src = this.tarjeta;
+      a.href = "#";
+      card.appendChild(a);
+      a.appendChild(img);
+      
+      return card;
+    }
   }
 
-  const peliculaAleatoria = seleccionarPeliculaAleatoria(data.suggested)
+  const mifuncion = async () => {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/Manuelard03/GHIFLIX-FINAL/main/assets/json/home.json"
+    );
+    const data = await response.json();
+    const primercarrusel = document.querySelectorAll(".imagenes");
+    const segundocarrusel = document.querySelectorAll(".imagenes2");
+    
+    for (let movie of data.buscados) {
+      let peli = new Pelicula(movie.card, movie.id);
+      const pelis = peli.render();
+  
+      primercarrusel.forEach((carrusel) => {
+        carrusel.appendChild(pelis.cloneNode(true));
+      });
+    }
+    for (let movie of data.viendo) {
+        let peli = new Pelicula(movie.card, movie.id);
+        const pelis = peli.render();
+    
+        segundocarrusel.forEach((favorito) => {
+          favorito.appendChild(pelis.cloneNode(true));
+        });
+      }
 
-  const newMainMovie = new Pelicula(
-    1,
-    mainMovie.banner,
-    mainMovie.banner_title,
-    mainMovie.synopsis
-  );
-  newMainMovie.innerHTML = `
-    <div class="botones">
-      <button role="button" class="boton">
-        <i class="fa-solid fa-play"></i>Reproducir
-      </button>
-      <button role="button" class="boton">
-        <i class="fa-regular fa-bookmark"></i>Guardar
-      </button>
-      <button class="adult">+18</button>
-    </div>
-  `;
-  const descripcion = document.createElement("p");
-  const titulo_banner = document.createElement("h3");
-  const img_title = document.createElement("img");
-  descripcion.classList.add("descripcion");
-  titulo_banner.classList.add("titulo");
-  img_title.src = newMainMovie.banner_title;
-  descripcion.innerHTML = `
-    ${newMainMovie.synopsis}
-  `;
-  newMainMovie.appendChild(descripcion);
-  newMainMovie.appendChild(titulo_banner);
-  titulo_banner.appendChild(img_title);
-  newMainMovie.style.background = `linear-gradient(rgba(0, 0, 255, 0.166), rgba(0, 0, 0, 0.855)), url(${newMainMovie.banner})`;*/  
-
-  /*const bannerRefresh = peliRandom(data.suggested);
-  const movieBanner = new Pelicula(
-    bannerRefresh.synopsis,
-    bannerRefresh.banner,
-    bannerRefresh.banner_title
-  );
-  mainMovie.appendChild(movieBanner.bannerRefresh());*/
-
-  for (let movie of data.suggested) {
-    let peli = new Pelicula(movie.card, movie.banner, movie.id, movie.banner_title, movie.synopsis);
-    const pelis = peli.render();
-
-    carruseles.forEach((carrusel) => {
-      carrusel.appendChild(pelis.cloneNode(true));
-    });
-  }
-
-  for (let movie of data.favourites) {
-    let peli = new Pelicula(movie.card, movie.banner, movie.id, movie.banner_title, movie.synopsis);
-    const pelis = peli.render();
-
-    favoritos.forEach((favorito) => {
-      favorito.appendChild(pelis.cloneNode(true));
-    });
-  }
-
-  for (let peli of data.suggested) {
-    let tarjeta = document.getElementById(peli.id);
-    tarjeta.addEventListener('click', () => {
-      const mainMovie = document.querySelector('.main-movie');
-      mainMovie.style.backgroundColor = 'linear-gradient(rgba(0, 0, 255, 0.166), rgba(0, 0, 0, 0))';
-      mainMovie.innerHTML = `
-      <div class="botones"><button role="button" class="boton"><i class="fa-solid fa-play"></i>Reproducir</button>
-      <button role="button" class="boton"><i class="fa-regular fa-bookmark"></i>Guardar</button>
-      <button class="adult">+18</button></div>
-      `;
-      const descripcion = document.createElement("p");
-      const titulo_banner = document.createElement("h3");
-      const img_title = document.createElement("img");
-      descripcion.classList.add("descripcion");
-      titulo_banner.classList.add("titulo");
-      img_title.src = peli.banner_title;
-      descripcion.innerHTML = `
-        ${peli.synopsis}
-        `;
-      mainMovie.appendChild(descripcion);
-      mainMovie.appendChild(titulo_banner);
-      titulo_banner.appendChild(img_title);
-      mainMovie.style.background = `linear-gradient(rgba(0, 0, 255, 0.166), rgba(0, 0, 0, 0.855)), url(${peli.banner})`;
-      mainMovie.style.backgroundSize = 'cover';
-      return
-    });
-  };
-
-  for (let peli of data.favourites) {
-    let tarjeta = document.getElementById(peli.id);
-    tarjeta.addEventListener('click', () => {
-      const mainMovie = document.querySelector('.main-movie');
-      mainMovie.style.backgroundColor = 'linear-gradient(rgba(0, 0, 255, 0.166), rgba(0, 0, 0, 0))';
-      mainMovie.innerHTML = `
-      <div class="botones"><button role="button" class="boton"><i class="fa-solid fa-play"></i>Reproducir</button>
-      <button role="button" class="boton"><i class="fa-regular fa-bookmark"></i>Guardar</button>
-      <button class="adult">+18</button></div>
-      `;
-      const descripcion = document.createElement("p");
-      const titulo_banner = document.createElement("h3");
-      const img_title = document.createElement("img");
-      descripcion.classList.add("descripcion");
-      titulo_banner.classList.add("titulo");
-      img_title.src = peli.banner_title;
-      descripcion.innerHTML = `
-        ${peli.synopsis}
-        `;
-      mainMovie.appendChild(descripcion);
-      mainMovie.appendChild(titulo_banner);
-      titulo_banner.appendChild(img_title);
-      mainMovie.style.background = `linear-gradient(rgba(0, 0, 255, 0.166), rgba(0, 0, 0, 0.855)), url(${peli.banner})`;
-      mainMovie.style.backgroundSize = 'cover';
-      return
-    });
-  };
-
-  mainMovie.style.backgroundColor = 'linear-gradient(rgba(0, 0, 255, 0.166), rgba(0, 0, 0, 0))';
-};
-
-mifuncion();
+    };
+    
+    mifuncion();
